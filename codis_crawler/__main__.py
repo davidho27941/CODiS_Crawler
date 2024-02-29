19fc0de4280510f6736d55576bf97bead5001330
davidho27941 <davidho@gapp.nthu.edu.tw>
2024-02-29 23:35:18 +0800

    feat: add command and arguments for selenium crawler.

--- codis_crawler/__main__.py
+++ codis_crawler/__main__.py
@@ -1,15 +1,82 @@
 import click
 
+from .core import (
+    seleniumCrawler,
+)
 from .utils import install_web_driver
 
+
 @click.group()
-def main():
-    ...
-    
+def main(): ...
+
 
 @main.command()
 def install_driver():
     install_web_driver()
-    
-if __name__ == '__main__':
+
+
+@main.command()
+@click.option(
+    "--mode",
+    "mode",
+    default="local",
+    type=click.Choice(["remote", "local"]),
+    help="Specify to run in remote or local environment.",
+)
+@click.option(
+    "--url",
+    "url",
+    default=None,
+    type=str,
+    help="Specify the url of remote chrome executor.",
+)
+@click.option(
+    "--headless",
+    is_flag=True,
+    default=False,
+    help="Run in headless mode. Default: False.",
+)
+@click.option(
+    "--area",
+    "area",
+    default=None,
+    type=click.Choice(["北區", "中區", "南區", "東區", "外島"]),
+    help="The city where weathre station located.",
+)
+@click.option(
+    "--city",
+    "city",
+    type=str,
+    default=None,
+    help="The city where weathre station located.",
+)
+@click.option("--date", "date", type=str, default=None, help="The date to download.")
+@click.option(
+    "--station", "station", type=str, required=True, help="The id of station."
+)
+def selenium_crawler(
+    mode: str,
+    url: str,
+    area: str,
+    city: str,
+    date: str,
+    station: str,
+    headless: bool,
+):
+    if mode == "remote" and url is None:
+        raise ValueError("Select to run in remote mode but no URL provided!")
+
+    crawler = seleniumCrawler(
+        mode=mode,
+        target_area=area,
+        targte_date=date,
+        target_station=station,
+        headless=headless,
+        target_city=city,
+        remote_url=url,
+    )
+    crawler.get_weather_data()
+
+
+if __name__ == "__main__":
     main()
